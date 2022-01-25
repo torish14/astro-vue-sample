cd22d705344fb22b15338837339d6a623e97df75
torish14 <nanamin.104@icloud.com>
2022-01-25 18:28:29 +0900

    ✨ feat: vue で Counterコンポーネントを作成

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="counter">
+		<button @click="decrement()">-</button>
+		<pre>{{ count }}</pre>
+		<button @click="increment()">+</button>
+	</div>
+	<div class="counter-message">
+		<slot />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
